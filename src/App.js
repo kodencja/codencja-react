60726815085142a523f6ce4b64211c6cec40400f
kodencja <krzysztof.lalik.pl@gmail.com>
2022-03-18 12:59:10 +0100

    Skip intro in App.js by setting transEnd to true at the beginning

--- src/App.js
+++ src/App.js
@@ -4,16 +4,17 @@ import "./css/anims.css";
 import "./fontello/css/fontello.css";
 import "./icons/font/flaticon.css";
 import Curtain from "./componentsHook/Curtain";
+import FrontPage from "./componentsHook/FrontPage";
 import Navbar from "./componentsHook/Navbar";
 
-let FrontPage;
+// let FrontPage;
 
 function App() {
-  const [transEnd, setTransEnd] = useState(false);
+  const [transEnd, setTransEnd] = useState(true);
 
-  useEffect(() => {
-    FrontPage = lazy(() => import("./componentsHook/FrontPage"));
-  }, [transEnd]);
+  // useEffect(() => {
+  //   FrontPage = lazy(() => import("./componentsHook/FrontPage"));
+  // }, [transEnd]);
 
   return (
     <React.Fragment>
